1374754d3bcf2f2c8508a84fcf01c1499ab33dee
Nate Tan <nktan93@gmail.com>
2019-08-21 20:50:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  return Array.prototype.map(function (driver) {
+  return Array.map(function (driver) {
     driver.toLowerCase();
   });
 }
