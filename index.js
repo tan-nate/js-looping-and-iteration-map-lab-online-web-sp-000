162be87e8987d5d91bb42f494bd5b8d1c71b4c57
Nate Tan <nktan93@gmail.com>
2019-08-21 21:25:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(names) {
+  return names.map(function (name) {
+    return Object.assign({}, { firstName: name.split(" ")[0], lastName: name.split(" ")[1] },);
+  });
+}
+
+function attributesToPhrase(names) {
+  return names.map(function (name) {
+    return name.name + ' is from ' + name.hometown
+  });
+}
